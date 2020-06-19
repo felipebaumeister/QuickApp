ae884bb5022b704bf7ce15da14a9c15ba622adc8
Leandro Carvalho <leandroeu_resgate@hotmail.com>
2020-06-19 01:02:12 -0300

    Validação formulario - up

--- src/app/home/signup/user-not-taken.validator.service.ts
+++ src/app/home/signup/user-not-taken.validator.service.ts
@@ -5,17 +5,19 @@ import { debounceTime, switchMap, map, first } from 'rxjs/operators';
 import { SignupService } from './signup.service';
 
 @Injectable({ providedIn: 'root' })
-export class UserNotTekenValidatorService{
+export class UserNotTekenValidatorService {
 
-    constructor(private SignupService: SignupService){
+    constructor(private SignupService: SignupService) {
 
     }
 
     ckeckUserNameTaken() {
         return (control: AbstractControl) => {
-           return control.valueChanges
-           .pipe(debounceTime(300))
-           .pipe(switchMap(userName => this.SignupService.checkUserNameTaken(userName)))
-           .pipe(map(isTaken => isTaken  ?  { userNameTaken: true } : null))
-           .pipe(first());
+            return control.valueChanges
+                .pipe(debounceTime(300))
+                .pipe(switchMap(userName => this.SignupService.checkUserNameTaken(userName)))
+                .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
+                .pipe(first());
+        }
+    }
 }
